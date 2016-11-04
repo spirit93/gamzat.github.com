2540e38df1721cb9da6b8b06d17ecf20690c75d9
Gamzat <3ggamzat@mail.ru>
2016-11-05 01:52:47 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /*** USE THIS FILE TO SET OPTIONS ***/
 
 setParams({
-    graphFile : "Gimbatov_Graph.GEXF",
+    graphFile : "Gimbatov_Graph.gexf",
         /*
             The GEXF file to show ! -- can be overriden by adding
             a hash to the document location, e.g. index.html#celegans.gexf
